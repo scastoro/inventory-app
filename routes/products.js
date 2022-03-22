7beb75eda2cd99ac51619654653c88da346b8ec0
Sal Castoro <20549092+scastoro@users.noreply.github.com>
2022-03-22 11:18:45 -0400

    Add routes and views for add new product
    Add basic styling for forms. Add ability to update image URL in update
    form routing and view.

--- routes/products.js
+++ routes/products.js
@@ -9,14 +9,42 @@ router.get('/', function(req, res, next) {
     res.render('all_products', { products: products });
   });
 });
+// Add new product
+router.get('/add', async function(req, res, next) {
+  const categories = await Category.find({});
+  res.render('add_product', { categories: categories });
+});
+
+router.post('/add', function(req, res, next) {
+  const productDetail = {
+    name: req.body.name,
+    description: req.body.description,
+    number_in_stock: req.body['in-stock'],
+    price: req.body.price,
+    category: [req.body.category],
+    image: req.body.image,
+  };
+
+  const product = new Product(productDetail);
+
+  product.save(function(err) {
+    if (err) {
+      throw err;
+    }
+    console.log('New Product: ' + product);
+  });
+  res.redirect('/products');
+});
+
 router.get('/:productId', function(req, res, next) {
-  Product.find({ _id: req.params.productId })
+  Product.findById(req.params.productId)
     .populate('category')
     .exec(function(err, product) {
       if (err) throw err;
-      res.render('product', { product: product[0] });
+      res.render('product', { product: product });
     });
 });
+
 // Populate edit products page with info from database
 router.get('/edit/:productId', async function(req, res, next) {
   const product = await Product.findById(req.params.productId).populate('category');
@@ -25,6 +53,7 @@ router.get('/edit/:productId', async function(req, res, next) {
   console.log(product.category[0].id);
   res.render('edit_product', { product: product, categories: categories });
 });
+
 // Send form data to database
 router.post('/edit/:productId', async function(req, res, next) {
   Product.findByIdAndUpdate(
@@ -35,6 +64,7 @@ router.post('/edit/:productId', async function(req, res, next) {
       number_in_stock: req.body['in-stock'],
       price: req.body.price,
       category: [req.body.category],
+      image: req.body.image,
     },
     function(err, docs) {
       if (err) throw err;
@@ -43,4 +73,5 @@ router.post('/edit/:productId', async function(req, res, next) {
   );
   res.redirect('/products/' + req.params.productId);
 });
+
 module.exports = router;
